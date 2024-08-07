7dc73de85616142e201c5e3f70bd391279740297
nitya <75457173+nityapisolkar@users.noreply.github.com>
2024-08-07 15:20:50 -0700

    functionality for delete icon

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-// function addTask() {
-//     var taskInput = document.getElementById("taskInput");
-//     var taskText = taskInput.value.trim();
-
-//     if (taskText !== "") {
-//         var taskList = document.getElementById("taskList");
-//         var newTaskItem = document.createElement("li");
-//         newTaskItem.classList.add("task-item");
-
-//         var completeButton = document.createElement("span");
-//           completeButton.textContent = " ";
-//           completeButton.classList.add("complete-btn");
-//           completeButton.onclick = function() {
-//             moveTaskToCompleted(newTaskItem);
-//         };
-
-//         newTaskItem.appendChild(completeButton);
-
-//         var taskTextSpan = document.createElement("span");
-//         taskTextSpan.textContent = taskText;
-//         newTaskItem.appendChild(taskTextSpan);
-
-//         taskList.appendChild(newTaskItem);
-
-//         taskInput.value = "";
-//     }
-// }
-
-// function moveTaskToCompleted(taskItem) {
-//     var completedItems = document.getElementById("completedItems");
-//     var completedTask = taskItem.cloneNode(true);
-//       completedTask.classList.add("completed-item");
-
-//     //adding that task to completed items list
-//       completedItems.appendChild(completedTask);
-
-//     //removing the task from the original todo list
-//     taskItem.parentNode.removeChild(taskItem);
-// }
-
 let taskIdCounter = 0;
 
 function addTask() {
@@ -74,7 +34,7 @@ function addTask() {
         var trashIcon = document.createElement("i");
         trashIcon.classList.add("fa-solid", "fa-trash");
         trashIcon.onclick = function() {
-            //logic for delete (gonna add this later)
+            deleteTask(newTaskItem);
         };
 
         iconsDiv.appendChild(pencilIcon);
@@ -126,5 +86,9 @@ function moveTaskBackToTodo(taskItem) {
     };
 
     taskList.appendChild(todoTask);
+    taskItem.parentNode.removeChild(taskItem);
+}
+
+function deleteTask(taskItem) {
     taskItem.parentNode.removeChild(taskItem);
 }
